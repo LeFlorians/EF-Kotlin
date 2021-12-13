214b1a8e31c68d0fb8e314792907ba5f5a716218
Le Florians <leflorians@protonmail.com>
2021-12-13 11:56:22 +0100

    started implementing DES

---
+++ src/main/kotlin/crypto/DES.kt
@@ -0,0 +1,25 @@
+package crypto
+
+class DES {
+
+    // Define static functions
+    companion object {
+
+        fun encrypt(text: Long, key: Long) {
+
+        }
+
+        // Initial permutation
+        private fun IP() {
+
+        }
+
+        // Final Permutation
+        private fun FP(LPT: Int, RPT: Int) : Long {
+
+            return 0;
+        }
+
+    }
+
+}
